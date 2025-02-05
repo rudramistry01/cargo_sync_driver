f1ca7ae708a1616c25e7cc122efbcad77ee33324
rudramistry01 <2203396160149@gmail.com>
2025-02-05 13:56:01 +0530

    nitisha changes

--- lib/5 screens/home/home_screen.dart
+++ lib/5 screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cargo_sync_driver/6%20global%20widgets/text_wideget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,7 +14,7 @@ class HomeScreen extends StatelessWidget {
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.orange,
               ),
               child: Text(
@@ -25,7 +26,7 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             ListTile(
-              leading: Icon(Icons.home),
+              leading: const Icon(Icons.home),
               title: Text(
                 'Home',
                 style: TextStyle(fontSize: 16.sp),
@@ -38,7 +39,7 @@ class HomeScreen extends StatelessWidget {
               },
             ),
             ListTile(
-              leading: Icon(Icons.settings),
+              leading: const Icon(Icons.settings),
               title: Text(
                 'Settings',
                 style: TextStyle(fontSize: 16.sp),
@@ -51,7 +52,7 @@ class HomeScreen extends StatelessWidget {
               },
             ),
             ListTile(
-              leading: Icon(Icons.info),
+              leading: const Icon(Icons.info),
               title: Text(
                 'About',
                 style: TextStyle(fontSize: 16.sp),
@@ -66,22 +67,88 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                _scaffoldKey.currentState?.openDrawer();
-              },
-            ),
-            SizedBox(
-                height: 20.h), // Add some space between the icon and the text
-            Text(
-              'Tap the icon to open the drawer',
-              style: TextStyle(fontSize: 16.sp),
-            ),
+            15.horizontalSpace,
+            Container(
+              height: 300.sp,
+              color: Colors.orange,
+              child: Padding(
+                padding: EdgeInsets.all(16.0.sp),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    15.verticalSpace,
+                    IconButton(
+                      icon: const Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        _scaffoldKey.currentState?.openDrawer();
+                      },
+                    ),
+                    const CustomText(
+                      text: "Welcome",
+                      textType: TextType.heading,
+                      textWeight: TextWeight.semiBold,
+                      color: Colors.white,
+                    ),
+                    const CustomText(
+                      text: "Rudra Mistry",
+                      textType: TextType.heading,
+                      textWeight: TextWeight.semiBold,
+                      color: Colors.white,
+                    ),
+                    10.verticalSpace,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        10.verticalSpace,
+                        const CustomText(
+                          text: "DriverID :- ",
+                          textType: TextType.bodyLarge,
+                          textWeight: TextWeight.semiBold,
+                          color: Colors.white,
+                        ),
+                        5.horizontalSpace,
+                        const CustomText(
+                          text: "6355311375",
+                          textType: TextType.bodyLarge,
+                          textWeight: TextWeight.semiBold,
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        10.verticalSpace,
+                        const CustomText(
+                          text: "Vehicle ID :- ",
+                          textType: TextType.bodyLarge,
+                          textWeight: TextWeight.semiBold,
+                          color: Colors.white,
+                        ),
+                        5.horizontalSpace,
+                        const CustomText(
+                          text: "6355311375",
+                          textType: TextType.bodyLarge,
+                          textWeight: TextWeight.semiBold,
+                          color: Colors.white,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
